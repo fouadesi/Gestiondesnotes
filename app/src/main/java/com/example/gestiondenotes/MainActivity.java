59865f99e1ac8939c89c6f8e4813533b741d699f
fouadesi <53347470+fouadesi@users.noreply.github.com>
2020-05-09 23:29:09 +0100

    Initial commit

--- app/src/main/java/com/example/gestiondenotes/MainActivity.java
+++ app/src/main/java/com/example/gestiondenotes/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.gestiondenotes;
+
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -11,7 +12,9 @@ import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.FirebaseDatabase;
+
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -21,7 +24,8 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
-import androidx.appcompat.widget.Toolbar ;
+
+import androidx.appcompat.widget.Toolbar;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
@@ -33,6 +37,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -44,26 +49,27 @@ import static com.example.gestiondenotes.R.string.ouvrir_nav;
 
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private androidx.appcompat.widget.Toolbar toolbar ;
-    private FirebaseAuth mAuth ;
+    private androidx.appcompat.widget.Toolbar toolbar;
+    private FirebaseAuth mAuth;
     private FloatingActionButton add_groupe_btn;
     private ListView listView;
-    private Button btn ;
-    DatabaseReference mDatabase ;
-    ArrayList<Module_users> module_users ;
+    private Button btn;
+    DatabaseReference mDatabase;
+    ArrayList<Module_users> module_users;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.ex_menu,menu);
-        return true ;
+        menuInflater.inflate(R.menu.ex_menu, menu);
+        return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case R.id.Disconnect: {
                 mAuth.signOut();
-                Intent i = new  Intent (MainActivity.this,Login.class);
+                Intent i = new Intent(MainActivity.this, Login.class);
                 startActivity(i);
                 finish();
                 break;
@@ -71,9 +77,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
         return true;
     }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mAuth = FirebaseAuth.getInstance();
 
         // tester l'utilisateur si il est connecte
@@ -81,28 +89,31 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             Intent i = new Intent(MainActivity.this, Login.class);
             startActivity(i);
             finish();
+            return;
         }
         listView = findViewById(R.id.listview);
- /// Affichage des modules
+        /// Affichage des modules
         DatabaseReference db_ref = FirebaseDatabase.getInstance().
                 getReference().child("Module_users").child(mAuth.getCurrentUser().getUid());
         db_ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                module_users= new ArrayList<>();
-                for (DataSnapshot dataSnapshot1:dataSnapshot.getChildren()) {
+                module_users = new ArrayList<>();
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
                     Module_users m = dataSnapshot1.getValue(Module_users.class);
-                   module_users.add(m);
-               }
-               moduleadapter adapter =new moduleadapter(MainActivity.this,module_users);
+                    module_users.add(m);
+                }
+                moduleadapter adapter = new moduleadapter(MainActivity.this, module_users);
                 listView.setAdapter(adapter);
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
-            }});
+            }
+        });
         // relier l'adapter avec les donnes
-        moduleadapter adapter= new moduleadapter(this,module_users);
+        moduleadapter adapter = new moduleadapter(this, module_users);
         adapter.notifyDataSetChanged();
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
@@ -120,24 +131,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 final TextInputEditText Nom_du_module = mView.findViewById(R.id.nom_du_module_dialog_bar);
                 final TextInputEditText Note_eliminatoire_du_module = mView.findViewById(R.id.note_eliminatoire_du_module);
                 final TextInputEditText Coefficient_du_module = mView.findViewById(R.id.coefficient_du_module);
-                final  TextInputEditText test1 = mView.findViewById(R.id.test1_dialog_bar);
-                final  TextInputEditText test2 = mView.findViewById(R.id.test2_dialog_bar);
-                final TextInputEditText absence = mView.findViewById(R.id.abscence_dialog_bar);
-                final TextInputEditText Participation = mView.findViewById(R.id.participation_dialog_bar);
                 builder.setPositiveButton("Ajouter", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(final DialogInterface dialog, int which) {
                         final String Nom = Nom_du_module.getText().toString().trim();
                         final String Noteelim = Note_eliminatoire_du_module.getText().toString().trim();
                         final String coeff = Coefficient_du_module.getText().toString().trim();
-                        final String test1_t = test1.getText().toString().trim();
-                        final String test2_t = test2.getText().toString().trim();
-                        final String absence_t = absence.getText().toString().trim();
-                        final String Participation_t = Participation.getText().toString().trim();
 
-                        if ((Nom.isEmpty() || Noteelim.isEmpty() || coeff.isEmpty() || test1_t.isEmpty() || test2_t.isEmpty() ||
-                                absence_t.isEmpty() || Participation_t.isEmpty()))  {
-                            final Snackbar s =  Snackbar.make(findViewById(android.R.id.content),
+
+                        if (Nom.isEmpty() || Noteelim.isEmpty() || coeff.isEmpty()) {
+                            final Snackbar s = Snackbar.make(findViewById(android.R.id.content),
                                     "Vous devez remplir les champs", Snackbar.LENGTH_LONG);
                             s.setDuration(10000);
                             s.setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_SLIDE);
@@ -147,29 +150,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                             s.setAction("OK", new View.OnClickListener() {
                                 @Override
                                 public void onClick(View v) {
-                                    s.dismiss(); }
+                                    s.dismiss();
+                                }
                             });
                             s.show();
                             return;
-                         }
-                         if (Double.valueOf(test1_t) + Double.valueOf(test2_t) +
-                                 Double.valueOf(absence_t) + Double.valueOf(Participation_t) != 100) {
-                             final Snackbar s =  Snackbar.make(findViewById(android.R.id.content),
-                                     "Vous devez remplir les champs", Snackbar.LENGTH_LONG);
-                             s.setDuration(10000);
-                             s.setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_SLIDE);
-                             s.setBackgroundTint(getResources().getColor(R.color.colorAccent));
-                             s.setTextColor(getResources().getColor(R.color.colorPrimary));
-                             s.setActionTextColor(getResources().getColor(R.color.colorPrimary));
-                             s.setAction("OK", new View.OnClickListener() {
-                                 @Override
-                                 public void onClick(View v) {
-                                     s.dismiss(); }
-                             });
-                             s.show();
-                             return;
-                         }
-                        add_groupe(Nom,Noteelim,coeff,test1_t,test2_t,absence_t,Participation_t); }
+                        }
+
+                        add_groupe(Nom, Noteelim, coeff);
+                    }
                 });
                 builder.setNegativeButton("Quitter", new DialogInterface.OnClickListener() {
                     @Override
@@ -180,38 +169,50 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 builder.setView(mView);
                 builder.show();
             }
-        }); }
+        });
+    }
 
     // insertion
-    void add_groupe(String nom,String Note_eliminatoire, String Coeff,String test1,String test2,String absence , String patrticipation) {
-         Module_users module_users = new Module_users(nom,Note_eliminatoire,Coeff,test1,test2,absence,patrticipation);
-         String key = mDatabase.child("Module_users").push().getKey();
-         module_users.setId(key);
+    void add_groupe(String nom, String Note_eliminatoire, String Coeff) {
+        Module_users module_users = new Module_users(nom, Note_eliminatoire, Coeff);
+        String key = mDatabase.child("Module_users").push().getKey();
+        module_users.setId(key);
+        Formule f = new Formule("0", "0", "0", "0");
+        module_users.setFormule(f);
+        mDatabase.child("Module_users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).
+                child(key)
+                .child("formule").setValue(f);
         mDatabase.child("Module_users").child(mAuth.getCurrentUser().getUid()).child(key).
                 setValue(module_users).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-              if (task.isSuccessful()) {
-              final Snackbar s =  Snackbar.make(findViewById(android.R.id.content),"Module est inserer", Snackbar.LENGTH_LONG);
-              s.setDuration(10000);
-                  s.setActionTextColor(getResources().getColor(R.color.colorAccent));
-                  s.setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_SLIDE);
-                  s.setBackgroundTint(getResources().getColor(R.color.colorAccent));
-                  s.setTextColor(getResources().getColor(R.color.colorPrimary));
-              s.setAction("OK", new View.OnClickListener() {
-                  @Override
-                  public void onClick(View v) {
-                      s.dismiss(); }});
-              s.show();
-              } else {
-                  Snackbar s =  Snackbar.make(findViewById(android.R.id.content),task.getException().getMessage(), Snackbar.LENGTH_LONG);
-                  s.show();
-              }
-            }}); }
+                if (task.isSuccessful()) {
+                    final Snackbar s = Snackbar.make(findViewById(android.R.id.content), "Groupe est ajouté", Snackbar.LENGTH_LONG);
+                    s.setDuration(5000);
+                    s.setActionTextColor(getResources().getColor(R.color.colorAccent));
+                    s.setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_SLIDE);
+                    s.setBackgroundTint(getResources().getColor(R.color.colorAccent));
+                    s.setTextColor(getResources().getColor(R.color.colorPrimary));
+                    s.setAction("OK", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            s.dismiss();
+                        }
+                    });
+                    s.show();
+                } else {
+                    Snackbar s = Snackbar.make(findViewById(android.R.id.content), task.getException().getMessage(), Snackbar.LENGTH_LONG);
+                    s.show();
+                }
+            }
+        });
+    }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return false;
     }
+
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
     }
