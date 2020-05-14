949e98d64f27590709c0e6765583840d69313cf7
fouadesi <53347470+fouadesi@users.noreply.github.com>
2020-05-14 16:09:03 +0100

    modification ActivityEditGroupe

--- app/src/main/java/com/example/gestiondenotes/Editgroupe.java
+++ app/src/main/java/com/example/gestiondenotes/Editgroupe.java
@@ -1,7 +1,10 @@
 package com.example.gestiondenotes;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,21 +12,33 @@ import android.widget.EditText;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 public class Editgroupe extends AppCompatActivity {
-    EditText nom_groupe , niveau_groupe ;
+    TextInputEditText nom_groupe , niveau_groupe ;
     String nom , niveau, id_groupe ;
     Button edit_groupe ;
     DatabaseReference db_ref ;
+    Toolbar toolbar ;
+    DrawerLayout drawer ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editgroupe);
+        toolbar = findViewById(R.id.toolbar_edit_groupe);
+        nom =getIntent().getExtras().getString("nom");
+        toolbar.setTitle("Modifier le Groupes");
+        toolbar.setSubtitle("Groupe : " + nom);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_edit_groupe);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
         nom_groupe = findViewById(R.id.nom_du_groupe_edit);
         niveau_groupe =findViewById(R.id.niveau_du_groupe_edit);
-        nom =getIntent().getExtras().getString("nom");
         niveau = getIntent().getExtras().getString("niveau") ;
         id_groupe = getIntent().getExtras().getString("id");
         nom_groupe.setText(nom);
